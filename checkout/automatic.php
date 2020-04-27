08e14c493e10a4002db28fd361520d5a6f9152d5
Yashgolflan <yash.khandelwal@golflan.com>
2020-04-27 16:44:18 +0530

    test payment integartion razorpay

---
+++ checkout/automatic.php
@@ -0,0 +1,69 @@
+<!--  The entire list of Checkout fields is available at
+ https://docs.razorpay.com/docs/checkout-form#checkout-fields -->
+
+<!-- <form action="verify.php" method="POST">
+  <script
+    src="https://checkout.razorpay.com/v1/checkout.js"
+    data-key="<?php echo $data['key']?>"
+    data-amount="<?php echo $data['amount']?>"
+    data-currency="INR"
+    data-name="<?php echo $data['name']?>"
+    data-image="<?php echo $data['image']?>"
+    data-description="<?php echo $data['description']?>"
+    data-prefill.name="<?php echo $data['prefill']['name']?>"
+    data-prefill.email="<?php echo $data['prefill']['email']?>"
+    data-prefill.contact="<?php echo $data['prefill']['contact']?>"
+    data-notes.shopping_order_id="3456"
+    data-order_id="<?php echo $data['order_id']?>"
+    <?php if ($displayCurrency !== 'INR') { ?> data-display_amount="<?php echo $data['display_amount']?>" <?php } ?>
+    <?php if ($displayCurrency !== 'INR') { ?> data-display_currency="<?php echo $data['display_currency']?>" <?php } ?>
+  >
+  </script> -->
+  <!-- Any extra fields to be submitted with the form but not sent to Razorpay -->
+  <!-- <input type="hidden" name="shopping_order_id" value="3456">
+</form> -->
+
+<!-- Card verification complimentary -->
+<button id="rzp-button1">Pay with Razorpay</button>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<form name='razorpayform' action="verify.php" method="POST">
+    <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id">
+    <input type="hidden" name="razorpay_signature"  id="razorpay_signature" >
+    <input type="hidden" name="pre-auth" id="pre-auth" value="pre-auth">
+</form>
+<script>
+// Checkout details as a json
+var options = <?php echo $json?>;
+console.log((options));
+/**
+ * The entire list of Checkout fields is available at
+ * https://docs.razorpay.com/docs/checkout-form#checkout-fields
+ */
+options.handler = function (response){
+    document.getElementById('razorpay_payment_id').value = response.razorpay_payment_id;
+    document.getElementById('razorpay_signature').value = response.razorpay_signature;
+    document.razorpayform.submit();
+};
+
+// Boolean whether to show image inside a white frame. (default: true)
+options.theme.image_padding = false;
+
+options.modal = {
+    ondismiss: function() {
+        console.log("This code runs when the popup is closed");
+    },
+    // Boolean indicating whether pressing escape key
+    // should close the checkout form. (default: true)
+    escape: true,
+    // Boolean indicating whether clicking translucent blank
+    // space outside checkout form should close the form. (default: false)
+    backdropclose: false
+};
+
+var rzp = new Razorpay(options);
+
+document.getElementById('rzp-button1').onclick = function(e){
+    rzp.open();
+    e.preventDefault();
+}
+</script>
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$keyId = 'rzp_test_i4gdNoZqKkzqj5';
+$keySecret = '1v2PsCL36GeHPH4jMI0poFMJ';
+
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
